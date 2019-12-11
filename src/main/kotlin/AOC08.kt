7b655be56f15aee0ce75eeac75bcb0b39130182e
Kuba Juszczak <kubajuszczak@gmail.com>
2019-12-11 22:01:34 +0000

    Refactored day 8

--- src/main/kotlin/AOC08.kt
+++ src/main/kotlin/AOC08.kt
@@ -1,4 +1,3 @@
-import java.lang.IllegalArgumentException
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -27,22 +26,14 @@ private fun part2(input: String): String {
         .chunked(25 * 6)
         .reduce { upperLayer, lowerLayer ->
             upperLayer.zip(lowerLayer).map {
-                when (it) {
-                    Pair('0', '0') -> '0'
-                    Pair('0', '1') -> '0'
-                    Pair('0', '2') -> '0'
-                    Pair('1', '0') -> '1'
-                    Pair('1', '1') -> '1'
-                    Pair('1', '2') -> '1'
-                    Pair('2', '0') -> '0'
-                    Pair('2', '1') -> '1'
-                    Pair('2', '2') -> '2'
-                    else -> throw IllegalArgumentException()
+                when (it.first) {
+                    '2' -> it.second
+                    else -> it.first
                 }
             }.joinToString(separator = "")
         }
         .chunked(25)
 
-    return image.toString().replace(",", "\n").replace("0", " ").replace("1", "█")
+    return image.toString().replace(",", "\n").replace("0", " ").replace("1", "▓")
 
 }
