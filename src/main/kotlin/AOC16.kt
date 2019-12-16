3a83b976ab0dba1f2d8e41c4e1effb86c0a0d7ce
Kuba Juszczak <kubajuszczak@gmail.com>
2019-12-16 23:12:19 +0000

    added day 16 part 2

--- src/main/kotlin/AOC16.kt
+++ src/main/kotlin/AOC16.kt
@@ -8,6 +8,9 @@ fun main() {
         println(AOC16.part1(input))
     }.also { println("${it}ms") }
 
+    measureTimeMillis {
+        println(AOC16.part2(input))
+    }.also { println("${it}ms") }
 }
 
 class AOC16 {
@@ -20,10 +23,28 @@ class AOC16 {
             return seq.slice(0 until 8).joinToString("")
         }
 
+        fun part2(input: String): String {
+            val skip = input.substring(0 until 7).toInt()
+            val elementCount = input.length * 10000 - skip
+
+            var seq = input.map { it.toString().toInt() }
+
+            // take the remainder + even multiples of input
+            // so that the list length is input * 10000 - offset
+            seq = seq.drop(skip % input.length) + (0 until elementCount / input.length).flatMap { seq }
+
+            for (i in 1..100) {
+                //assumption: the offset is so far into the message that any digit before has a 0 multiplier
+                // and any digit after is a 1
+                seq = fft2(seq)
+            }
+
+            return seq.slice(0 until 8).joinToString("")
+        }
+
         private fun fft(input: List<Int>): List<Int> {
             return input.indices.map { index ->
                 val sequence = getSequence(index).drop(1).iterator()
-
                 input.map { it * sequence.next() }.sum().let { abs(it) % 10 }
             }.toList()
         }
@@ -32,6 +53,19 @@ class AOC16 {
             val base = listOf(0, 1, 0, -1)
 
             return base.flatMap { baseValue -> (0..index).map { baseValue } }.asSequence().repeat()
+        }
+
+        private fun fft2(input: List<Int>): List<Int> {
+            // in matrix form this would be an upper triangular matrix with all 1s along the diagonal and above
+            // essentially sums from the current digit to the end
+
+            val sums = ArrayList<Int>()
+            var last = 0
+            for (n in input.reversed()) {
+                sums.add(n + last)
+                last += n
+            }
+            return sums.reversed().map { it % 10 }
         }
     }
 }
--- src/test/kotlin/AOC16Tests.kt
+++ src/test/kotlin/AOC16Tests.kt
@@ -4,23 +4,44 @@ import org.junit.jupiter.api.Test
 class AOC16Tests {
 
     @Test
-    fun `Check that after 100 FFT rounds on 80871224585914546619083218645595 the first 8 digits are 24176176`(){
+    fun `Check that after 100 FFT rounds on 80871224585914546619083218645595 the first 8 digits are 24176176`() {
         val expected = "24176176"
         val actual = AOC16.part1("80871224585914546619083218645595")
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `Check that after 100 FFT rounds on 19617804207202209144916044189917 the first 8 digits are 73745418`(){
+    fun `Check that after 100 FFT rounds on 19617804207202209144916044189917 the first 8 digits are 73745418`() {
         val expected = "73745418"
         val actual = AOC16.part1("19617804207202209144916044189917")
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `Check that after 100 FFT rounds on 69317163492948606335995924319873 the first 8 digits are 52432133`(){
+    fun `Check that after 100 FFT rounds on 69317163492948606335995924319873 the first 8 digits are 52432133`() {
         val expected = "52432133"
         val actual = AOC16.part1("69317163492948606335995924319873")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Check that after 100 FFT rounds on 03036732577212944063491565474664 x10000 the result is 84462026`() {
+        val expected = "84462026"
+        val actual = AOC16.part2("03036732577212944063491565474664")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Check that after 100 FFT rounds on 02935109699940807407585447034323 x10000 the result is 78725270`() {
+        val expected = "78725270"
+        val actual = AOC16.part2("02935109699940807407585447034323")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Check that after 100 FFT rounds on 03081770884921959731165446850517 x10000 the result is 53553731`() {
+        val expected = "53553731"
+        val actual = AOC16.part2("03081770884921959731165446850517")
         assertEquals(expected, actual)
     }
 }
